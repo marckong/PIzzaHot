7cd8a713031a0846880a6d1c84d73f574ae71cdd
James Wingert <jameswingert1@gmail.com>
2022-09-10 16:32:43 -0700

    Setup frontend template with Next.js, Tailwind, Jest, and others

--- app/pizza/settings.py
+++ app/pizza/settings.py
@@ -141,7 +141,6 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
