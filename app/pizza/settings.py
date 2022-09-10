fb0ec6369a76dd184338076e1804ab9ff6514811
James Wingert <jameswingert1@gmail.com>
2022-09-10 15:50:54 -0700

    Proxy still not working correctly

--- app/pizza/settings.py
+++ app/pizza/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
+SECRET_KEY = os.environ.get("SECRET_KEY", "foobar")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get("DEBUG", 0)))
