82c8ff8c6ead4c565d4b4541279200178a62b89c
James Wingert <jameswingert1@gmail.com>
2022-09-11 02:22:10 -0700

    Fixing cords issue with Django

--- app/pizza/settings.py
+++ app/pizza/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
-from .DEFAULT import DEFAULT_HEADERS
+from .DEFAULTS import DEFAULT_HEADERS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
