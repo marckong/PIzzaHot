9073479f0214b3af39b0a49cebcfe9cdfd373d48
James Wingert <jameswingert1@gmail.com>
2022-09-11 06:20:03 -0700

    Fixed cors issue (again)

--- app/pizza_api/urls.py
+++ app/pizza_api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from pizza_api.views import PizzaCreate, PizzaList, PizzaUpdate, PizzaDelete
+from pizza_api.views import PizzaCreate, PizzaList, PizzaUpdate, PizzaDelete, PizzaDetail
 
 urlpatterns = [
     path("pizza", PizzaList.as_view()),
     path("pizza/create", PizzaCreate.as_view()),
     path("pizza/<int:pk>", PizzaUpdate.as_view()),
+    path("pizza/<int:pk>", PizzaDetail.as_view()),
     path("pizza/<int:pk>/delete", PizzaDelete.as_view()),
 ]
--- app/toppings_api/urls.py
+++ app/toppings_api/urls.py
@@ -5,11 +5,13 @@ from toppings_api.views import (
     ToppingsList,
     ToppingsUpdate,
     ToppingsDelete,
+    ToppingsDetail
 )
 
 urlpatterns = [
     path("toppings", ToppingsList.as_view()),
     path("toppings/create", ToppingsCreate.as_view()),
     path("toppings/<int:pk>", ToppingsUpdate.as_view()),
+    path("toppings/<int:pk>", ToppingsDetail.as_view()),
     path("toppings/<int:pk>/delete", ToppingsDelete.as_view()),
 ]
