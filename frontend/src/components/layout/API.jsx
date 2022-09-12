9037f430b0fe58b184b6fa210893c5738f807007
James Wingert <jameswingert1@gmail.com>
2022-09-11 21:26:39 -0700

    Fixed linting issues

--- frontend/src/components/layout/API.jsx
+++ frontend/src/components/layout/API.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://18.144.103.106/',
+  baseURL: 'https://18.144.103.106/',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
